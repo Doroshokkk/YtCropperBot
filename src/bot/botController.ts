d12b3695736fa92e3cac8e5cfeb8f268c31b6440
Vlad Doroshenko <vladorchikz@gmail.com>
2024-03-11 23:04:38 +0200

    move bot logic to a separate module

---
+++ src/bot/botController.ts
@@ -0,0 +1,25 @@
+import { Telegraf } from "telegraf";
+import { cropFromStart, cropSong, cropToEnd, getFullSong, handleNumberInput, handleOtherInput, respondToYoutubeLink } from "./botService";
+import * as dotenv from "dotenv";
+dotenv.config();
+const { TOKEN } = process.env;
+
+export const setupBot = () => {
+    const bot = new Telegraf(TOKEN);
+
+    bot.hears(/.*youtube\.com.*|.*youtu\.be.*/, respondToYoutubeLink);
+
+    bot.action("full", getFullSong);
+
+    bot.action("crop", cropSong);
+
+    bot.hears("Start", cropFromStart);
+
+    bot.hears("End", cropToEnd);
+
+    bot.hears(/\d+/, handleNumberInput);
+
+    bot.hears(/.*/, handleOtherInput);
+
+    bot.launch();
+};
---
+++ src/bot/botService.ts
@@ -0,0 +1,163 @@
+import { userSessions } from "../utils/userSessions";
+import { replyWithAudioPopulated } from "../utils/replyWithAudioPopulated";
+import { downloadFullSong, downloadCroppedSong } from "../utils/apiUtils";
+
+export const respondToYoutubeLink = (ctx) => {
+    const chatId = ctx.message.chat.id;
+    const keyboard = {
+        reply_markup: {
+            inline_keyboard: [[{ text: "Full", callback_data: "full" }], [{ text: "Crop", callback_data: "crop" }]],
+        },
+    };
+
+    userSessions.set(chatId, {
+        videoUrl: ctx.message.text,
+        startSecond: 0,
+        endSecond: 0,
+        state: undefined,
+    });
+
+    // Set the state to expect the start timecode
+    userSessions.get(chatId).state = "start";
+
+    ctx.reply("Choose an option:", keyboard);
+};
+
+export const getFullSong = async (ctx) => {
+    const chatId = ctx.update.callback_query.message.chat.id;
+    const videoUrl = userSessions.get(chatId)?.videoUrl;
+
+    ctx.reply("Loading...");
+    try {
+        const response = await downloadFullSong(videoUrl);
+        console.log("data after success", response.headers);
+        replyWithAudioPopulated(ctx, response);
+    } catch (error) {
+        console.error("Error calling API:", error.message);
+        ctx.reply("Error calling the API. Please try again later.");
+    }
+};
+
+export const cropSong = (ctx) => {
+    const keyboard = {
+        reply_markup: {
+            keyboard: [[{ text: "Start" }]],
+            one_time_keyboard: false,
+        },
+    };
+
+    ctx.reply("Enter start time: ", keyboard);
+};
+
+export const cropFromStart = (ctx) => {
+    const chatId = ctx.message.chat.id;
+    const userSession = userSessions.get(chatId);
+
+    if (!userSession || userSession.state !== "start") {
+        ctx.reply("Invalid session. Please start again.");
+        return;
+    }
+
+    userSession.startSecond = "start";
+    userSession.state = "end";
+
+    const keyboard = {
+        reply_markup: {
+            keyboard: [[{ text: "End" }]],
+            one_time_keyboard: true, // Hide the keyboard after a button is pressed
+        },
+    };
+
+    ctx.reply("Enter end time: ", keyboard);
+};
+
+export const cropToEnd = async (ctx) => {
+    const chatId = ctx.message.chat.id;
+    const userSession = userSessions.get(chatId);
+
+    if (!userSession || userSession.state !== "end") {
+        ctx.reply("Invalid session. Please start again.");
+        return;
+    }
+
+    userSession.endSecond = "end";
+
+    const { videoUrl, startSecond, endSecond } = userSession;
+
+    ctx.reply("Loading...");
+
+    try {
+        const response = await downloadCroppedSong(videoUrl, startSecond, endSecond);
+        console.log("data", response.headers);
+        replyWithAudioPopulated(ctx, response);
+    } catch (error) {
+        console.error("Error calling API:", error.message);
+        ctx.reply("Error calling the API. Please try again later.");
+    }
+
+    userSessions.delete(chatId);
+};
+
+export const handleNumberInput = async (ctx) => {
+    const chatId = ctx.message.chat.id;
+    const userSession = userSessions.get(chatId);
+
+    if (!userSession) {
+        ctx.reply("Invalid session. Please start again.");
+        return;
+    }
+
+    if (userSession.state === "start") {
+        userSession.startSecond = parseFloat(ctx.message.text);
+        ctx.reply("enter a number maaaan");
+
+        const keyboard = {
+            reply_markup: {
+                keyboard: [[{ text: "End" }]],
+                one_time_keyboard: true, // Hide the keyboard after a button is pressed
+            },
+        };
+
+        ctx.reply("Please provide the end timecode (in seconds):", keyboard);
+        userSession.state = "end";
+    } else if (userSession.state === "end") {
+        userSession.endSecond = parseFloat(ctx.message.text);
+        ctx.reply("Enter a number pls man");
+
+        const { videoUrl, startSecond, endSecond } = userSession;
+
+        ctx.reply("Loading...");
+        try {
+            const response = await downloadCroppedSong(videoUrl, startSecond, endSecond);
+
+            console.log("data", response.headers);
+
+            replyWithAudioPopulated(ctx, response);
+        } catch (error) {
+            console.error("Error calling API:", error.message);
+            ctx.reply("Error calling the API. Please try again later.");
+        }
+
+        userSessions.delete(chatId);
+    }
+};
+
+export const handleOtherInput = (ctx) => {
+    const chatId = ctx.message.chat.id;
+    const userSession = userSessions.get(chatId);
+
+    if (userSession && userSession.state) {
+        if (userSession.state === "start") {
+            ctx.reply("Enter a starting seconds number pls man");
+            return;
+        }
+
+        if (userSession.state === "end") {
+            ctx.reply("Enter ending seconds number pls man");
+            return;
+        }
+        //cancel behaviour
+    }
+    console.log(ctx.message?.chat);
+    ctx.reply("Hello");
+};
