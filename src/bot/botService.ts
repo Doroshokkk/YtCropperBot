dc01efb930133a178156ca2bf71bdd9f6dc73175
Vlad Doroshenko <vladorchikz@gmail.com>
2024-03-14 20:55:28 +0200

    use new session module based on Redis instead of a map

--- src/bot/botService.ts
+++ src/bot/botService.ts
@@ -1,9 +1,16 @@
-import { userSessions } from "../utils/userSessions";
+import {
+    userSessions,
+    initCropSession,
+    getCropSesssionData,
+    getVideoUrl,
+    clearCropSession,
+    setCropSessionField,
+} from "../utils/userSessions";
 import { replyWithAudioPopulated } from "../utils/replyWithAudioPopulated";
 import { downloadFullSong, downloadCroppedSong } from "../utils/apiUtils";
 import { timeStringToSeconds } from "../utils/secondsConverter";
 
-export const respondToYoutubeLink = (ctx) => {
+export const respondToYoutubeLink = async (ctx) => {
     const chatId = ctx.message.chat.id;
     const keyboard = {
         reply_markup: {
@@ -11,28 +18,21 @@ export const respondToYoutubeLink = (ctx) => {
         },
     };
 
-    userSessions.set(chatId, {
-        videoUrl: ctx.message.text,
-        startSecond: 0,
-        endSecond: 0,
-        state: undefined,
-    });
-
-    // Set the state to expect the start timecode
-    userSessions.get(chatId).state = "start";
+    await initCropSession(chatId, ctx.message.text);
 
     ctx.reply("Choose an option:", keyboard);
 };
 
 export const getFullSong = async (ctx) => {
     const chatId = ctx.update.callback_query.message.chat.id;
-    const videoUrl = userSessions.get(chatId)?.videoUrl;
+    const videoUrl = await getVideoUrl(chatId);
 
     ctx.reply("Loading...");
     try {
         const response = await downloadFullSong(videoUrl);
         console.log("data after success", response.headers);
         replyWithAudioPopulated(ctx, response);
+        clearCropSession(chatId);
     } catch (error) {
         console.error("Error calling API:", error.message);
         ctx.reply("Error calling the API. Please try again later.");
@@ -40,6 +40,7 @@ export const getFullSong = async (ctx) => {
 };
 
 export const cropSong = (ctx) => {
+    const chatId = ctx.update.callback_query.message.chat.id;
     const keyboard = {
         reply_markup: {
             keyboard: [[{ text: "Start" }]],
@@ -47,20 +48,25 @@ export const cropSong = (ctx) => {
         },
     };
 
+    setCropSessionField(chatId, "state", "start");
+
     ctx.reply("Enter start time (in plain seconds or MM:SS format): ", keyboard);
 };
 
-export const cropFromStart = (ctx) => {
+export const cropFromStart = async (ctx) => {
     const chatId = ctx.message.chat.id;
-    const userSession = userSessions.get(chatId);
+    const userSession = await getCropSesssionData(chatId);
 
     if (!userSession || userSession.state !== "start") {
         ctx.reply("Invalid session. Please start again.");
         return;
     }
 
-    userSession.startSecond = "start";
-    userSession.state = "end";
+    setCropSessionField(chatId, "startSecond", "start");
+    setCropSessionField(chatId, "state", "end");
+
+    // userSession.startSecond = "start";
+    // userSession.state = "end";
 
     const keyboard = {
         reply_markup: {
@@ -74,29 +80,32 @@ export const cropFromStart = (ctx) => {
 
 export const cropToEnd = async (ctx) => {
     const chatId = ctx.message.chat.id;
-    const userSession = userSessions.get(chatId);
+    const userSession = await getCropSesssionData(chatId);
 
     if (!userSession || userSession.state !== "end") {
         ctx.reply("Invalid session. Please start again.");
         return;
     }
 
-    userSession.endSecond = "end";
+    setCropSessionField(chatId, "endSecond", "end");
 
-    const { videoUrl, startSecond, endSecond } = userSession;
+    // userSession.endSecond = "end";
+
+    const { videoUrl, startSecond } = userSession;
 
     ctx.reply("Loading...");
 
     try {
-        const response = await downloadCroppedSong(videoUrl, startSecond, endSecond);
+        const response = await downloadCroppedSong(videoUrl, startSecond, "end");
         console.log("data", response.headers);
         replyWithAudioPopulated(ctx, response);
+        clearCropSession(chatId);
     } catch (error) {
         console.error("Error calling API:", error.message);
         ctx.reply("Error calling the API. Please try again later.");
     }
 
-    userSessions.delete(chatId);
+    clearCropSession(chatId);
 };
 
 export const handleNumberInput = async (ctx) => {
