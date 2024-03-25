ab5c20e00f95995978f127d68cda71c183bc218c
Vlad Doroshenko <vladorchikz@gmail.com>
2024-03-25 20:12:35 +0200

    track downloaded songs for an hour properly, set expiration

--- src/utils/rateLimiter.ts
+++ src/utils/rateLimiter.ts
@@ -2,8 +2,13 @@ import { redis } from "../redis/redisClient";
 
 export async function countDownloadedSongs(chatId: number) {
     try {
-        const downloadedSongs = await redis.hget(chatId.toString(), "songsDownloaded");
-        await redis.hmset(`${chatId}-info`, `songsDownloaded`, downloadedSongs ? parseInt(downloadedSongs) + 1 : 1);
+        const songsDownloaded = await redis.hget(`${chatId}-info`, "songsDownloaded");
+        if (songsDownloaded) {
+            await redis.hincrby(`${chatId}-info`, "songsDownloaded", 1);
+        } else {
+            await redis.hmset(`${chatId}-info`, `songsDownloaded`, 1);
+            await redis.expire(`${chatId}-info`, 3600);
+        }
     } catch (error) {
         console.error("error setting to redis", error);
     }
