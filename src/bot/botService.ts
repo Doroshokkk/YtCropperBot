7a3b7df1a22565d18e6e7e0cfd8f8a8eebce76b7
Vlad Doroshenko <vladorchikz@gmail.com>
2024-04-06 16:55:47 +0300

    added start command handling, saving links to downloaded songs

--- src/bot/botService.ts
+++ src/bot/botService.ts
@@ -5,6 +5,23 @@ import { timeStringToSeconds } from "../utils/secondsConverter";
 import { cancelKeyboard, endingKeyboard, inlineCropKeyboard, startingKeyboard } from "../utils/keyboards";
 import { Context } from "telegraf";
 import { reachedDownloadLimit } from "../utils/rateLimiter";
+import { addDownloadedSong, setUser } from "../mongo/services/userService";
+
+export const firstMessage = async (ctx: Context) => {
+    try {
+        ctx.reply(
+            "Hey, welcome! This bot can crop songs and stuff, later there will be an instruction but I'm lazy for now to write it ¯\\_(ツ)_/¯",
+        );
+        //@ts-ignore
+        const { id, first_name, username } = ctx.message.chat;
+        console.log(ctx.message.chat);
+
+        setUser({ tg_id: id, username, first_name });
+    } catch (error) {
+        ctx.reply("Bot must be down currently =( \n Please stick around and try in some time!");
+        console.error("Error creating user:", error.message);
+    }
+};
 
 export const respondToYoutubeLink = async (ctx: Context) => {
     try {
@@ -39,7 +56,8 @@ export const getFullSong = async (ctx: Context) => {
         const response = await downloadFullSong(videoUrl);
         console.log("data after success", response.headers);
         replyWithAudioPopulated(ctx, response);
-        clearCropSession(chatId);
+        await clearCropSession(chatId);
+        await addDownloadedSong(chatId, videoUrl);
     } catch (error) {
         console.error("Error calling API:", error.message);
         ctx.reply("Error calling the API. Please try again later.");
@@ -88,13 +106,14 @@ export const cropToEnd = async (ctx: Context) => {
         const response = await downloadCroppedSong(videoUrl, startSecond, "end");
         console.log("data", response.headers);
         replyWithAudioPopulated(ctx, response);
-        clearCropSession(chatId);
+        await clearCropSession(chatId);
+        await addDownloadedSong(chatId, videoUrl);
     } catch (error) {
         console.error("Error calling API:", error.message);
         ctx.reply("Error calling the API. Please try again later.");
     }
 
-    clearCropSession(chatId);
+    await clearCropSession(chatId);
 };
 
 export const handleNumberInput = async (ctx: Context) => {
@@ -131,12 +150,12 @@ export const handleNumberInput = async (ctx: Context) => {
             const response = await downloadCroppedSong(videoUrl, startSecond, endSecond);
             console.log("data", response.headers);
             replyWithAudioPopulated(ctx, response);
+            await clearCropSession(chatId);
+            await addDownloadedSong(chatId, videoUrl);
         } catch (error) {
             console.error("Error calling API:", error.message);
             ctx.reply("Error calling the API. Please try again later.");
         }
-
-        clearCropSession(chatId);
     }
 };
 
@@ -164,5 +183,5 @@ export const handleOtherInput = async (ctx: Context) => {
 export const handleCancellation = async (ctx: Context) => {
     const chatId = ctx.message.chat.id;
     ctx.reply("Sure, cancelled the cropping");
-    clearCropSession(chatId);
+    await clearCropSession(chatId);
 };
